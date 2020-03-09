b376cff1bffcffa59e82b203cb27e4739e53d40d
Kirill <KirillGrinev@gmail.com>
2020-03-09 22:33:31 +0200

    add description for XOR solutions

--- 3.OddOccurrencesInArray /solution.js
+++ 3.OddOccurrencesInArray /solution.js
@@ -1,3 +1,15 @@
+// XOR (Exclusive Or) -  is a logical operation that outputs true only
+// when inputs differ (one is true, the other is false)
+// How it works with array [9, 3, 9, 3, 9, 7, 9] :
+// 9 = 1001
+// 3 = 0011
+// 9 = 1001
+// 3 = 0011
+// 9 = 1001
+// 7 = 0111
+// 9 = 1001
+//     0111 - even number 1 in the column is 0, odd - 1
+
 function solution(A) {
   let result = 0;
 
@@ -8,4 +20,4 @@ function solution(A) {
   return result
 }
 
-solution([9.5, 3, 9.5, 3, 9, 7, 9]);
+solution([9, 3, 9, 3, 9, 7, 9]);
