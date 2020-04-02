9211c049897f5c22c881c81895e5505576857ad0
Kirill <KirillGrinev@gmail.com>
2020-04-03 02:04:25 +0300

    finish "PermMissingElem" with 100% result

--- 5.PermMissingElem/solution.js
+++ 5.PermMissingElem/solution.js
@@ -1,5 +1,19 @@
 function solution(A){
+  let length = A.length;
+  let sum = (length + 1) * (length + 2) / 2;
+
+  let currentArrSum = 0;
+
+  if(A.length !== 0){
+    currentArrSum = A.reduce((prevVal, currentVal)=>{
+      return prevVal + currentVal;
+    });
+  }
+
+  let result = sum - currentArrSum;
+
+  console.log(result);
 
 }
 
-solution(A);
+solution([2, 3, 1, 5]);
